43121e24f1244e037c5a2b44a68c4bad44d1fdcd
Anton Komarov <anton.komarov@gmail.com>
2012-02-12 21:53:38 +0400

    Remove ffmpeg as a splitter. Use mp3splt instead. Need testing.

--- split_record.sh
+++ split_record.sh
@@ -12,6 +12,7 @@ fname2=${fname%.*}
 yymmdd=${fname:0:8}
 hh=${fname:8:2}
 record_file="$scannerhome/${yymmdd}/${fname2}.mp3"
+mp3spltopts="-q"
 
 test $# -eq 0 && exit 1
 test -f $scannerlog || exit 1
@@ -26,22 +27,29 @@ do
                 group=$(echo $line | awk -F, '{print $2}')
                 channel=$(echo $line | awk -F, '{print $3}')
                 freq=$(echo $line | awk -F, '{print $4}')
-                ref_epoch_time=$(echo $line | awk -F, '{print $9}')
-                start_time=$(echo $line | awk -F, '{print $10}')
-                end_time=$(echo $line | awk -F, '{print $11}')
-                ss=$(echo "scale=2; $start_time-$ref_epoch_time$correction" | bc)
-                if [ "${ss:0:1}" == "-" -o "${ss:0:1}" == "." ]; then ss=0; fi
-                t=$(echo "scale=2; $end_time-$start_time" | bc)
-                if [ "${t:0:1}" == "." ]; then t=0; fi
-                ti=$(echo $t | awk -F. '{print $1}')
-		filename_date_part=$(date -d "Jan 1, 1970 00:00:00 +0000 + $start_time seconds" +%Y%m%d%H%M%S)
-		filename="${filename_date_part}_${freq}.mp3"
+                r0=$(echo $line | awk -F, '{print $9}')
+                s0=$(echo $line | awk -F, '{print $10}')
+                e0=$(echo $line | awk -F, '{print $11}')
+                s=$(echo "scale=2; $s0-$r0$correction" | bc)
+                e=$(echo "scale=2; $e0-$r0$correction" | bc)
+		ss=$(echo $s | awk -F. '{print $1}')
+		sh=$(echo $s | awk -F. '{print $2}')
+		es=$(echo $e | awk -F. '{print $1}')
+		eh=$(echo $e | awk -F. '{print $2}')
+		[ "X$ss" == "X" ] && ss=0
+		[ "X$es" == "X" ] && es=0
+		sm=$(expr $ss / 60)
+		ss=$(expr $ss % 60)
+		em=$(expr $es / 60)
+		es=$(expr $es % 60)
+		filename_date_part=$(date -d @$s0 +%Y%m%d%H%M%S)
+		filename="${filename_date_part}_${freq}@t.mp3"
 	        dir1="${scannerhome}/${yymmdd}/${system}/${group}/${channel}/${hh}"
 		test "X$group" == "X" && dir1="${scannerhome}/${yymmdd}/${system}/${freq}/${hh}"
 		[[ "$freq" =~ $mhz ]] || dir1="${scannerhome}/${yymmdd}/${system}/${freq}/${hh}"
                 test -d $dir1 || mkdir -p $dir1
-		test $ti -gt 2 && ffmpeg -y -ss $ss -t $t -i $record_file -acodec copy "${dir1}/${filename}" 2>/dev/null
-		test $ti -gt 2 && echo "Splitting ${filename_date_part}_${freq}.mp3 from $record_file to ${dir1}, position $ss, duration $t.">> /tmp/split.log
+		test $ti -gt 2 && mp3splt $mp3spltopts $record_file $sm.$ss.$sh $em.$es.$eh -o "${dir1}/${filename}" 2>/dev/null
+		test $ti -gt 2 && echo "Splitting ${filename_date_part}_${freq}.mp3 from $record_file to ${dir1}, start $sm.$ss.$sh, end $em.$es.$eh.">> /tmp/split.log
 	fi
 	prevline="$line"
 	sleep 1s
