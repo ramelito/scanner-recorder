3bc95a2d820e5c056bb1091e1c448b6931b82fe6
Anton Komarov <anton.komarov@gmail.com>
2012-02-12 19:33:43 +0400

    Make some cosmetic changes in if structures.

--- record0.sh
+++ record0.sh
@@ -3,6 +3,9 @@
 export LANG=C
 export PATH=/opt/bin:$PATH
 
+res=""
+mdl="^MDL*"
+
 test -f /scanner_audio/record.cfg && cp /scanner_audio/record.cfg /opt/etc/record.cfg
 test -f /opt/etc/record.cfg && source /opt/etc/record.cfg
 test -f /opt/etc/record.cfg || exit 1
@@ -23,6 +26,8 @@ s0_imount=$(echo $scanner0 | awk -F";" '{print $7}')
 s0_profile=$(echo $scanner0 | awk -F";" '{print $8}')
 
 test "X$s0_rec" == "X" && s0_rec=0
+test "X$s0_scard" == "X" && s0_card=0
+[ $(arecord -l | grep "card $s0_scard:" | wc -l) -eq 1 ] || { echo "Card $s0_scard does not exist."; exit 1 }
 
 case "$s0_profile" in
         lq)
@@ -41,27 +46,32 @@ esac
 
 if [ $s0_type -eq 0 ]; then
 	echo "Scanner is UNcontrolled."
-	if [ "X$s0_scard" != "X" ]; then
-		if [ $(arecord -l | grep "card $s0_scard:" | wc -l) -eq 1 ]; then
-			echo "Scanner card $s0_scard exists."
-			if [ $s0_rec -eq 0 ]; then
-				echo "Let's start recording for uncontrolled scanner."
-				watchdog0.sh $s0_scard $s0_bitrate $s0_samplerate 1>/tmp/watchdog0.log &
-			fi
-			if [ $s0_rec -eq 1 -a "X$s0_ihost" != "X" -a "X$s0_ipass" != "X" -a "X$s0_imount" != "X" ]; then
-				watchdog1.sh $s0_scard $s0_bitrate $s0_samplerate $s0_ihost $s0_ipass $s0_imount 1>/tmp/watchdog1.log &
-			fi
-			if [ $s0_rec -eq 2 -a "X$s0_ihost" != "X" -a "X$s0_ipass" != "X" -a "X$s0_imount" != "X" ]; then
-				watchdog2.sh $s0_scard $s0_bitrate $s0_samplerate $s0_ihost $s0_ipass $s0_imount 1>/tmp/watchdog2.log &
-			fi
-		fi
+	if [ $s0_rec -eq 0 ]; then
+		echo "Simple recording."
+		watchdog0.sh $s0_scard $s0_bitrate $s0_samplerate 1>/tmp/watchdog0.log &
+	fi
+	if [ $s0_rec -eq 1 ]; then
+		echo "Simple recording and livecast."
+
+		test "X$s0_ihost" == "X" && { echo "Icecast server host not defined. Aborting."; exit 1 }
+		test "X$s0_ipass" == "X" && { echo "Icecast password not defined. Aborting."; exit 1 }
+		test "X$s0_imount" == "X" && { echo "Icecast mount not defined. Aborting."; exit 1 }
+
+		watchdog1.sh $s0_scard $s0_bitrate $s0_samplerate $s0_ihost $s0_ipass $s0_imount 1>/tmp/watchdog1.log &
+	fi
+	if [ $s0_rec -eq 2 ]; then
+		echo "Livecast only."
+
+		test "X$s0_ihost" == "X" && { echo "Icecast server host not defined. Aborting."; exit 1 }
+		test "X$s0_ipass" == "X" && { echo "Icecast password not defined. Aborting."; exit 1 }
+		test "X$s0_imount" == "X" && { echo "Icecast mount not defined. Aborting."; exit 1 }
+
+		watchdog2.sh $s0_scard $s0_bitrate $s0_samplerate $s0_ihost $s0_ipass $s0_imount 1>/tmp/watchdog2.log &
 	fi
 fi
 
 if [ $s0_type -eq 1 ]; then
 	echo "Scanner is controlled."
-	res=""
-	mdl="^MDL*"
 	while (true); do
 		test -L /dev/scanners/$s0_port && res=$(MDL -s$s0_port)
 		echo $res
@@ -72,16 +82,17 @@ if [ $s0_type -eq 1 ]; then
 		fi
 		sleep 10
 	done
-	if [ "X$s0_scard" != "X" ]; then
-		if [ $(arecord -l | grep "card $s0_scard:" | wc -l) -eq 1 ]; then
-			echo "Scanner card $s0_scard exists."
-			if [ $s0_rec -eq 0 ]; then
-				echo "Let's start recording for Uniden scanner."
-				watchdog_uniden0.sh $s0_port $s0_scard $s0_bitrate $s0_samplerate 1>/tmp/watchdog_uniden0.log &
-			fi
-			if [ $s0_rec -eq 1 -a "X$s0_ihost" != "X" -a "X$s0_ipass" != "X" -a "X$s0_imount" != "X" ]; then
-				watchdog_uniden1.sh $s0_port $s0_scard $s0_bitrate $s0_samplerate $s0_ihost $s0_ipass $s0_imount 1>/tmp/watchdog_uniden1.log &
-			fi
-		fi
+	if [ $s0_rec -eq 0 ]; then
+		echo "Recording for Uniden scanner."
+		watchdog_uniden0.sh $s0_port $s0_scard $s0_bitrate $s0_samplerate 1>/tmp/watchdog_uniden0.log &
+	fi
+	if [ $s0_rec -eq 1 ]; then
+		echo "Recording for Uniden scanner and livecast."
+
+		test "X$s0_ihost" == "X" && { echo "Icecast server host not defined. Aborting."; exit 1 }
+		test "X$s0_ipass" == "X" && { echo "Icecast password not defined. Aborting."; exit 1 }
+		test "X$s0_imount" == "X" && { echo "Icecast mount not defined. Aborting."; exit 1 }
+
+		watchdog_uniden1.sh $s0_port $s0_scard $s0_bitrate $s0_samplerate $s0_ihost $s0_ipass $s0_imount 1>/tmp/watchdog_uniden1.log &
 	fi
 fi
