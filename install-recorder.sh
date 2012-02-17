582e0b8e12a060cf1a26a6227af42bb0783c23fc
Anton Komarov <anton.komarov@gmail.com>
2012-02-17 11:56:21 +0000

    added stop_record.sh to install

--- install-recorder.sh
+++ install-recorder.sh
@@ -15,6 +15,7 @@ test -f code.sh && sudo cp code.sh $installpath || (echo "failed to install code
 test -f logscanner.sh && sudo cp logscanner.sh $installpath || (echo "failed to install logscanner.sh"; exit 1)
 test -f recordcleaner.sh && sudo cp recordcleaner.sh $installpath || (echo "failed to install recordcleaner.sh"; exit 1)
 test -f split_record.sh && sudo cp split_record.sh $installpath || (echo "failed to install split_record.sh"; exit 1)
+test -f stop_record.sh && sudo cp stop_record.sh $installpath || (echo "failed to install stop_record.sh"; exit 1)
 test -f usb_port_no.sh && sudo cp usb_port_no.sh $installpath || (echo "failed to install usb_port_no.sh"; exit 1)
 test -f watchdog0.sh && sudo cp watchdog0.sh $installpath || (echo "failed to install watchdog0.sh"; exit 1)
 test -f watchdog1.sh && sudo cp watchdog1.sh $installpath || (echo "failed to install watchdog1.sh"; exit 1)
@@ -22,6 +23,7 @@ test -f watchdog2.sh && sudo cp watchdog2.sh $installpath || (echo "failed to in
 test -f watchdog_uniden0.sh && sudo cp watchdog_uniden0.sh $installpath || (echo "failed to install watchdog_uniden0.sh"; exit 1)
 test -f watchdog_uniden1.sh && sudo cp watchdog_uniden1.sh $installpath || (echo "failed to install watchdog_uniden1.sh"; exit 1)
 test -f record.conf && sudo cp record.conf $configpath || (echo "failed to install record.cfg"; exit 1)
+test -f record0.sh && sudo cp record0.sh $installpath || (echo "failed to install record.sh"; exit 1)
 test -f record.sh && sudo cp record.sh /etc/init.d || (echo "failed to install record.sh"; exit 1)
 test -L /etc/rc2.d/S99record.sh || sudo ln -s etc/init.d/record.sh /etc/rc2.d/S99record.sh
 test -L /etc/rc3.d/S99record.sh || sudo ln -s /etc/init.d/record.sh /etc/rc3.d/S99record.sh
