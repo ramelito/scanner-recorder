360df4e42a05de72082dfdebdd6b36ab0d8bcc36
Anton Komarov <anton.komarov@gmail.com>
2012-02-18 21:19:11 +0000

    minor fixes

--- split_record.sh
+++ split_record.sh
@@ -12,7 +12,7 @@ fname2=${fname%.*}
 yymmdd=${fname:0:8}
 hh=${fname:8:2}
 record_file="$scannerhome/${yymmdd}/${fname2}.mp3"
-mp3spltopts="-q"
+mp3spltopts="-Q"
 
 test $# -eq 0 && exit 1
 test -f $scannerlog || exit 1
@@ -24,14 +24,14 @@ do
 	[[ "$line" =~ $comma ]] && continue
 	if [ "$prevline" != "$line" ]; then
 		system=$(echo $line | awk -F, '{print $1}')
-                group=$(echo $line | awk -F, '{print $2}')
-                channel=$(echo $line | awk -F, '{print $3}')
-                freq=$(echo $line | awk -F, '{print $4}')
-                r0=$(echo $line | awk -F, '{print $9}')
-                s0=$(echo $line | awk -F, '{print $10}')
-                e0=$(echo $line | awk -F, '{print $11}')
-                s=$(echo "scale=2; $s0-$r0$correction" | bc)
-                e=$(echo "scale=2; $e0-$r0$correction" | bc)
+        group=$(echo $line | awk -F, '{print $2}')
+        channel=$(echo $line | awk -F, '{print $3}')
+        freq=$(echo $line | awk -F, '{print $4}')
+        r0=$(echo $line | awk -F, '{print $9}')
+        s0=$(echo $line | awk -F, '{print $10}')
+        e0=$(echo $line | awk -F, '{print $11}')
+        s=$(echo "scale=2; $s0-$r0$correction" | bc)
+        e=$(echo "scale=2; $e0-$r0$correction" | bc)
 		ss=$(echo $s | awk -F. '{print $1}')
 		sh=$(echo $s | awk -F. '{print $2}')
 		es=$(echo $e | awk -F. '{print $1}')
@@ -43,13 +43,13 @@ do
 		em=$(expr $es / 60)
 		es=$(expr $es % 60)
 		filename_date_part=$(date -d @$s0 +%Y%m%d%H%M%S)
-		filename="${filename_date_part}_${freq}@t.mp3"
-	        dir1="${scannerhome}/${yymmdd}/${system}/${group}/${channel}/${hh}"
+		filename="${filename_date_part}_${freq}@t"
+	    dir1="${scannerhome}/${yymmdd}/${system}/${group}/${channel}/${hh}"
 		test "X$group" == "X" && dir1="${scannerhome}/${yymmdd}/${system}/${freq}/${hh}"
 		[[ "$freq" =~ $mhz ]] || dir1="${scannerhome}/${yymmdd}/${system}/${freq}/${hh}"
-                test -d $dir1 || mkdir -p $dir1
-		test $ti -gt 2 && mp3splt $mp3spltopts $record_file $sm.$ss.$sh $em.$es.$eh -o "${dir1}/${filename}" 2>/dev/null
-		test $ti -gt 2 && echo "Splitting ${filename_date_part}_${freq}.mp3 from $record_file to ${dir1}, start $sm.$ss.$sh, end $em.$es.$eh.">> /tmp/split.log
+#        test -d $dir1 || mkdir -p $dir1
+		mp3splt $mp3spltopts $record_file $sm.$ss.$sh $em.$es.$eh -d ${dir1} -o "${filename}" 2>/dev/null
+		echo "Splitting ${filename_date_part}_${freq}.mp3 from $record_file to ${dir1}, start $sm.$ss.$sh, end $em.$es.$eh.">> /tmp/split.log
 	fi
 	prevline="$line"
 	sleep 1s
--- usb_port_no.sh
+++ usb_port_no.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-number=$(echo $1 | awk -F"/" '{print $7}' | awk -F. '{print $2}')
+number=$(echo $1 | awk -F"/" '{print $8}' | awk -F. '{print $2}')
 echo $(expr $number - 1)
