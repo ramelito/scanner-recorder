2b5cf2eec72bac449810316feba1d572996781db
Anton Komarov <anton.komarov@gmail.com>
2012-02-19 11:27:40 +0000

    fix for tail, now overlapping for 2 files

--- split_record.sh
+++ split_record.sh
@@ -10,7 +10,7 @@ fname2=${fname%.*}
 yymmdd=${fname:0:8}
 hh=${fname:8:2}
 cutlinesfile="/tmp/cutlines${fname:18:1}"
-splitlog="/tmp/split${fname:18:1}"
+splitlog="/tmp/split${fname:18:1}.log"
 record_file="$scannerhome/${yymmdd}/${fname2}.mp3"
 mp3spltopts="-Q"
 numlines0=0
@@ -22,7 +22,7 @@ test -f $record_file || exit 1
 while (true)	
 do
 	numlines=$(cat $scannerlog | wc -l)
-    [ $numlines -gt $numlines0 ] && let cutlines=$numlines-$numlines0 || cutlines=0
+    [ $numlines -gt $numlines0 ] && let cutlines=$numlines-$numlines0+2 || cutlines=0
     numlines0=$numlines
     tail -n${cutlines} $scannerlog > $cutlinesfile 
     while read line; do
@@ -33,26 +33,29 @@ do
         r0=$(echo $line | awk -F, '{print $9}')
         s0=$(echo $line | awk -F, '{print $10}')
         e0=$(echo $line | awk -F, '{print $11}')
-        [ "X$e0" == "X" ] && continue
-        s=$(echo "scale=2; $s0-$r0$correction" | bc)
-        e=$(echo "scale=2; $e0-$r0$correction" | bc)
-		ss=$(echo $s | awk -F. '{print $1}')
-		sh=$(echo $s | awk -F. '{print $2}')
-		es=$(echo $e | awk -F. '{print $1}')
-		eh=$(echo $e | awk -F. '{print $2}')
-		[ "X$ss" == "X" ] && ss=0
-		[ "X$es" == "X" ] && es=0
-		sm=$(expr $ss / 60)
-		ss=$(expr $ss % 60)
-		em=$(expr $es / 60)
-		es=$(expr $es % 60)
-		filename_date_part=$(date -d @$s0 +%Y%m%d%H%M%S)
-		filename="${filename_date_part}_${freq}@t"
-	    dir1="${scannerhome}/${yymmdd}/${system}/${group}/${channel}/${hh}"
-		test "X$group" == "X" && dir1="${scannerhome}/${yymmdd}/${system}/${freq}/${hh}"
-		[[ "$freq" =~ $mhz ]] || dir1="${scannerhome}/${yymmdd}/${system}/${freq}/${hh}"
-		mp3splt $mp3spltopts $record_file $sm.$ss.$sh $em.$es.$eh -d ${dir1} -o "${filename}" 2>/dev/null
-		echo "Splitting ${filename_date_part}_${freq}.mp3 from $record_file to ${dir1}, start $sm.$ss.$sh, end $em.$es.$eh." >> $splitlog
+        if [ "X$e0" != "X" ]; then
+            s=$(echo "scale=2; $s0-$r0$correction" | bc)
+            e=$(echo "scale=2; $e0-$r0$correction" | bc)
+		    ss=$(echo $s | awk -F. '{print $1}')
+    		sh=$(echo $s | awk -F. '{print $2}')
+	    	es=$(echo $e | awk -F. '{print $1}')
+		    eh=$(echo $e | awk -F. '{print $2}')
+    		[ "X$ss" == "X" -o "${ss:0:1}" == 0 ] && ss=0
+	    	[ "X$es" == "X" -o "${es:0:1}" == 0 ] && es=0
+    		[ "X$sm" == "X" -o "${sm:0:1}" == 0 ] && sm=0
+	    	[ "X$em" == "X" -o "${em:0:1}" == 0 ] && em=0
+		    sm=$(expr $ss / 60)
+    		ss=$(expr $ss % 60)
+	    	em=$(expr $es / 60)
+		    es=$(expr $es % 60)
+    		filename_date_part=$(date -d @$s0 +%Y%m%d%H%M%S)
+	    	filename="${filename_date_part}_${freq}@t"
+	        dir1="${scannerhome}/${yymmdd}/${system}/${group}/${channel}/${hh}"
+    		test "X$group" == "X" && dir1="${scannerhome}/${yymmdd}/${system}/${freq}/${hh}"
+	    	[[ "$freq" =~ $mhz ]] || dir1="${scannerhome}/${yymmdd}/${system}/${freq}/${hh}"
+		    mp3splt $mp3spltopts $record_file $sm.$ss.$sh $em.$es.$eh -d ${dir1} -o "${filename}" 2>/dev/null
+    		echo "Splitting ${filename_date_part}_${freq}.mp3 from $record_file to ${dir1}, start $sm.$ss.$sh, end $em.$es.$eh." >> $splitlog
+        fi
     done < $cutlinesfile
 	sleep 10s
 done
